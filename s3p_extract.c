d018cf24446766a7c85ab8f1dd5ab35d15d2dfed
abstractedfox <coldcaption@gmail.com>
2025-01-21 21:36:31 -0500

    Add repacking (#4)
    * work started on repacking
    
    * repacking works!
    
    * cleanup
    
    * fix incorrect use of strcmp
    
    * fix off by 1
    
    * Incorporate feedback
    
    * add check for argument count when specifying an outfile name
    
    * Clean up arg parsing to handle bad inputs more gracefully

--- s3p_extract.c
+++ s3p_extract.c
@@ -26,7 +26,75 @@ struct entry {
 struct s3v0 {
     char magic[4]; // S3V0
     uint32_t filestart;
+    uint32_t length;
+    
+    uint8_t unknown[20];
 };
+
+
+void pack(int infile_count, char** infiles, char* out_filename){
+    printf("Packing %d files\n", infile_count);
+
+    FILE* out_f = fopen(out_filename, "wb");
+    if (!out_f){
+        printf("Couldn't create output file %s\n", out_filename);
+        return;
+    }
+
+    struct header h = {
+        .magic = "S3P0",
+        .entries = infile_count
+    };
+
+    fwrite(&h, sizeof(h), 1, out_f);
+
+    struct entry *entries = malloc(sizeof(struct entry) * infile_count);
+    memset(entries, 0, sizeof(struct entry) * infile_count);
+    fwrite(entries, sizeof(struct entry), infile_count, out_f);
+
+    for(int i = 0; i < infile_count; i++){
+        printf("Packing %s\n", infiles[i]);
+
+        entries[i].offset = ftell(out_f);
+        FILE* audio = fopen(infiles[i], "rb");
+        if (!audio){
+            printf("Error opening %s\n", infiles[i]);
+            fclose(out_f);
+            return;
+        }
+
+        int length = 0;
+        fseek(audio, 0, SEEK_END);
+        length = ftell(audio);
+        rewind(audio);
+
+        struct s3v0 audio_header= {
+            .magic = "S3V0",
+            .filestart = 0x20,
+            .length = length
+        };
+
+        fwrite(&audio_header, sizeof(struct s3v0), 1, out_f);
+
+        uint8_t* buffer = malloc(length);
+        fread(buffer, 1, length, audio);
+        fwrite(buffer, 1, length, out_f);
+
+        entries[i].length = ftell(out_f) - entries[i].offset;
+        
+        free(buffer);
+        fclose(audio);
+    }
+    
+    uint32_t endbytes = 0x12345678; //Unclear what this value is for
+    fwrite(&endbytes, sizeof(uint32_t), 1, out_f);
+
+    //Repopulate the entries now that they have relevant offsets
+    fseek(out_f, sizeof(struct header), 0);
+    fwrite(entries, sizeof(struct entry), infile_count, out_f);
+
+    fclose(out_f);
+}
 
 void convert(const char* path) {
     printf("%s\n", path);
@@ -93,10 +161,27 @@ void convert(const char* path) {
 }
 
 int main(int argc, char** argv) {
-    if(argc < 2) {
-        printf("Usage: s3p_extract file.s3p [file2.s3p] [file3.s3p]\n");
+    if(argc < 2 || strcmp(argv[argc - 1], "-o") == 0 || strcmp(argv[argc - 1], "-pack") == 0) {
+        printf("Usage:\n");
+        printf("Unpack: s3p_extract file.s3p [file2.s3p] [file3.s3p]\n");
+        printf("Repack: s3p_extract -pack [-o filename.s3p] file.wma [file2.wma] [file3.wma]\n"); 
         return 1;
     }
+
+    if (strcmp(argv[1], "-pack") == 0){
+        if (strcmp(argv[2], "-o") == 0){
+            if (argc < 5){
+                printf("Enter at least one input file\n");
+                return 1;
+            }
+            pack(argc - 4, &argv[4], argv[3]);
+        }
+        else{
+            pack(argc-2, &argv[2], "out.s3p");
+        }
+        return 0;
+    }
+
     for(int i = 1; i < argc; i++) {
         convert(argv[i]);
     }
